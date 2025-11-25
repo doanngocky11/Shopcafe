6b18c8cb3b9aaed1dc6ad51c9e6216cecd57730b
NguyenHuynhNhatDuy2004 <nhatduy3q@gmail.com>
2025-11-25 20:50:01 +0700

    up layout

--- app/src/main/java/com/example/shopcafe/activity/BaseActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.shopcafe.activity;
-import androidx.appcompat.app.AppCompatActivity;
-public class BaseActivity extends AppCompatActivity {
-
-}
--- app/src/main/java/com/example/shopcafe/helper/ChangeNumberItemsListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.shopcafe.helper;
-
-public interface ChangeNumberItemsListener {
-    void onCartDataChanged();
-}
